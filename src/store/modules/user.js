ecfd3e45555ad6d5c6a526bdb115b84b412b5e5f
rgp <rgp1466639716@126.com>
2024-07-24 14:32:13 +0800

    进入页面获取人员信息，保存redux

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,18 +5,22 @@ import { getToken, setToken as _setToken  } from "@/utils";
 const userStore = createSlice({
   name:"user",
   initialState:{
-    token: getToken() || ''
+    token: getToken() || '',
+    userInfo:{}
   },
   reducers:{
     setToken(state,action){
       state.token = action.payload
       // localstorage
       _setToken(action.payload)
+    },
+    setUserInfo(state,action){
+      state.userInfo = action.payload
     }
   }
 })
 
-const { setToken } = userStore.actions 
+const { setToken, setUserInfo } = userStore.actions 
 
 const fetchLogin = (loginForm) => {
   return async (dispatch) => {
@@ -24,7 +28,14 @@ const fetchLogin = (loginForm) => {
     dispatch(setToken(res.data.token))
   }
 }
+// 获取个人信息
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+    const res = await request.get('/user/profile')
+    dispatch(setUserInfo(res.data))
+  }
+}
 const userReducer = userStore.reducer
 
-export { fetchLogin, setToken } 
+export { fetchLogin, fetchUserInfo, setToken,setUserInfo } 
 export default userReducer
