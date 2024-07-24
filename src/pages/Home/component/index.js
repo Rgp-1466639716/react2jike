d86d48090570ccfa9032068764280cf1a1b1eb1c
rgp <rgp1466639716@126.com>
2024-07-24 15:26:12 +0800

    引入echarts，抽出图像公共组件

---
+++ src/pages/Home/component/index.js
@@ -0,0 +1,34 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
+const  BarChart = ({
+  title
+}) => {
+  const chartRef = useRef(null)
+  useEffect(()=>{
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+    const option = {
+      title: {
+        text:title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Auguarl']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+  },[])
+  return <div ref={chartRef} style={{height:400,width:500}}></div>
+}
+export default  BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,10 @@
+import BarChart from "./component"
+
 const Home = () => {
-  return <div>home</div>
+  
+  return <div>
+    <BarChart title='三大'/>
+    <BarChart title='三小'/>
+  </div>
 }
 export default Home
